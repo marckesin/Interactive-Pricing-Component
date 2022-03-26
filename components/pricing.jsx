6c4aaaec7cc9b2cc2834f8c4b56306f92231a225
marckesin <192.168.2.1@protonmail.com>
2022-03-26 20:31:20 -0300

    Definição de layout inicial

---
+++ components/pricing.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+export default function Pricing() {
+  return (
+    <div className={styles.card}>
+      Pageviews $ /month Monthly Billing Yearly Billing 25% discount Unlimited
+      websites 100% data ownership Email reports Start my trial
+    </div>
+  );
+}
