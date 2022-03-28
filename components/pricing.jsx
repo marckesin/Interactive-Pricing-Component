c9008be24887ed5bde7470aa2d651b3d630277d3
marckesin <192.168.2.1@protonmail.com>
2022-03-27 23:47:00 -0300

    Correção de propriedades inválidas

--- components/pricing.jsx
+++ components/pricing.jsx
@@ -1,11 +1,11 @@
+import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import { styled, width } from "@mui/system";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/system";
 import styles from "../styles/Home.module.css";
 import variables from "../styles/variables.module.css";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
 
 const StyledButton = styled(Button)({
   height: "40%",
@@ -29,7 +29,7 @@ const StyledButton = styled(Button)({
 
 const StyledStack = styled(Stack)({
   borderTop: `3px solid ${variables.lightGrayishBlue}`,
-  padding: "1.5rem 3rem",
+  padding: "1rem 3rem 2.5rem",
 
   "@media(max-width: 600px)": {
     flexDirection: "column",
@@ -40,10 +40,18 @@ const StyledListItemText = styled(ListItemText)({
   paddingLeft: "16px",
   margin: 0,
   color: variables.grayishBlue,
+
+  "@media(max-width: 600px)": {
+    flex: "0 0 auto",
+  },
 });
 
 const StyledListItem = styled(ListItem)({
   padding: "4px 0",
+
+  "@media(max-width: 600px)": {
+    justifyContent: "center",
+  },
 });
 
 // Card pricing component
@@ -54,13 +62,13 @@ export default function Pricing() {
       Pageviews $ /month Monthly Billing Yearly Billing 25% discount
       <StyledStack direction="row" spacing={2}>
         <List>
-          {list.map(item => (
-            <StyledListItem>
+          {list.map((item, index) => (
+            <StyledListItem key={index}>
               <svg xmlns="http://www.w3.org/2000/svg" width="9" height="8">
                 <path
                   fill="none"
                   stroke="#10D8C4"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="M1 4.134l1.907 1.908L7.949 1"
                 />
               </svg>
